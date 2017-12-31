5a301ae9d32ab8d706de0b8d44edb15fa588e7fc
Oluwafemi Sule <oluwafemisule@outlook.com>
2017-12-31 02:07:08 +0100

    update configuration entries [#3]

--- web/config.py
+++ web/config.py
@@ -1,11 +1,18 @@
 import os
 
 
-def getenv(key):
-    return os.getenv(key, '')
+def getenv(key, default=''):
+    return os.getenv(key, default)
 
 
 def configure_defaults():
-    return {
-        'PYFILE': os.path.join('..', '.env'),
+    env = getenv('ENVIRONMENT', 'development')
+    config = {
+        'DEBUG': getenv('DEBUG', True),
+        'REDIS_URL': getenv('REDIS_URL'),
+        'YOUTUBE_API_KEY': getenv('YOUTUBE_API_KEY'),
     }
+    if env != 'PRODUCTION':
+        config['PYFILE'] = os.path.join('..', '.env')
+    return config
+
