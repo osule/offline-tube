80680d9eb0d1a3f156211bea0eb4c35ce30967d5
Oluwafemi Sule <oluwafemisule@outlook.com>
2018-01-13 13:12:41 +0100

    add test environment to non environment file environments [#3]

--- web/config.py
+++ web/config.py
@@ -1,18 +1,19 @@
 import os
 
 
+NON_ENVFILE_ENVIRONMENTS = {'PRODUCTION', 'TEST'}
+
 def getenv(key, default=''):
     return os.getenv(key, default)
 
 
 def configure_defaults():
-    env = getenv('ENVIRONMENT', 'development')
+    env = getenv('ENVIRONMENT', 'DEVELOPMENT')
     config = {
         'DEBUG': getenv('DEBUG', True),
         'REDIS_URL': getenv('REDIS_URL'),
         'YOUTUBE_API_KEY': getenv('YOUTUBE_API_KEY'),
     }
-    if env != 'PRODUCTION':
+    if env not in NON_ENVFILE_ENVIRONMENTS:
         config['PYFILE'] = os.path.join('..', '.env')
     return config
-
