d0da01256ffdc1e6cdb3c9ac8c142a9587a988c4
Oluwafemi Sule <oluwafemisule@outlook.com>
2018-02-05 13:50:45 +0100

    style Search component [#4]

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import './Search.css';
 
 export default class Search extends Component
 {
     render() {
         return (
-            <div>
-                <input type="text" id="query"/>
-                <button type="submit" onSubmit={this.props.onSubmit}>Submit</button>
+            <div className="Search-container">
+                <input className="Search-input" type="text" id="query"/>
+                <button className="Search-button" type="submit" onSubmit={this.props.onSubmit}>Submit</button>
             </div>
         );
     }
