224330f9cbf32fe7b47fa5df0c785fc22c86e94f
Oluwafemi Sule <oluwafemisule@outlook.com>
2018-05-12 15:15:23 +0200

    rename module

--- web/celeree.py
+++ web/celeree.py
@@ -2,7 +2,7 @@ from celery import Celery
 
 celery = Celery('offline_tube')
 
-celery.config_from_object('web.celeryconfig')
+celery.config_from_object('celeryconfig')
 
 if __name__ == '__main__':
     celery.start()
