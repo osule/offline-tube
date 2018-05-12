019fe2ad15949a0ef258ef644c73165455dbff3e
Oluwafemi Sule <oluwafemisule@outlook.com>
2018-05-12 15:19:31 +0200

    Use streamline configuration file

--- web/factory.py
+++ web/factory.py
@@ -1,17 +1,9 @@
 from flask import Flask
 from flask_sse import sse
 
-from .config import configure_defaults
-
 
 def create_app():
     app = Flask(__name__)
-    config = configure_defaults()
-
-    app.config.from_object(config)
-    pyfile = config.get('PYFILE', None)
-
-    if pyfile:
-        app.config.from_pyfile(pyfile)
+    app.config.from_pyfile('config.py')
 
     return app, sse
