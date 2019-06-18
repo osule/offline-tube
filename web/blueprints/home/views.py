36b2e88dfb1d1cc10867f546fcc904cbb6ded167
Oluwafemi Sule <oluwafemisule@outlook.com>
2019-06-18 11:49:33 +0200

    Try sse

--- web/blueprints/home/views.py
+++ web/blueprints/home/views.py
@@ -1,12 +1,14 @@
 from os import getenv
 
-from flask import jsonify
+from flask import jsonify, url_for
 
 
 def create_views(blueprint):
     ''' Create views.'''
     @blueprint.route('/')
     def index():
+        stream = url_for('sse.stream')
+        return "<script> const source = new EventSource('{}');</script>".format(stream)
         api_version = getenv('API_VERSION')
         dct = {
             'app_name': 'Offline Tube', 
